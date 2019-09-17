20f442d07aad3c05a40ff678b073c3ca1125a021
ryucxd <ryucxd@gmail.com>
2019-09-17 10:33:15 +0100

    Add project files.

---
+++ DispatchCameraUploader/frmCameraUpload.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DispatchCameraUploader
+{
+    partial class frmCameraUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCameraUpload));
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rd_packing_bsc = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(206, 132);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(18, 106);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(263, 20);
+            this.txtSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source File:";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(0, 171);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(296, 23);
+            this.progressBar2.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(152, 33);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dispatch";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // rd_packing_bsc
+            // 
+            this.rd_packing_bsc.AutoSize = true;
+            this.rd_packing_bsc.Location = new System.Drawing.Point(12, 33);
+            this.rd_packing_bsc.Name = "rd_packing_bsc";
+            this.rd_packing_bsc.Size = new System.Drawing.Size(134, 17);
+            this.rd_packing_bsc.TabIndex = 5;
+            this.rd_packing_bsc.TabStop = true;
+            this.rd_packing_bsc.Text = "Packing BLACK SONY";
+            this.rd_packing_bsc.UseVisualStyleBackColor = true;
+            this.rd_packing_bsc.CheckedChanged += new System.EventHandler(this.Rd_packing_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(225, 33);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Slimline";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Packing NIKON";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(152, 56);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(67, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Welding ";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4_CheckedChanged);
+            // 
+            // frmCameraUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 204);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.rd_packing_bsc);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnUpload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCameraUpload";
+            this.Text = "Camera Upload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rd_packing_bsc;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
---
+++ DispatchCameraUploader/frmCameraUpload.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DispatchCameraUploader
+{
+    public partial class frmCameraUpload : Form
+    {
+        public string destination { get; set; }
+        public frmCameraUpload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar2.Minimum = 0;
+                progressBar2.Maximum = 100;
+               
+                progressBar2.Value = 0;
+
+                float totalFileCount;
+                float newFileCount;
+                float progressValue;
+                int progressValueInt;
+
+
+
+                string path = txtSource.Text;
+                var files = Directory.EnumerateFiles(path, "*.jpg", SearchOption.AllDirectories)
+                         .Select(fn => new FileInfo(fn));
+                var fileDateGroups = files.GroupBy(fi => fi.LastWriteTime.Date);
+
+                totalFileCount = files.Count();
+
+
+                
+
+                foreach (var dateGroup in fileDateGroups)
+                {
+                    string dir = Path.Combine(destination, dateGroup.Key.ToString("yyyy-MM-dd"));
+                    //string dir = Path.Combine(@"C:\Users\Aled\Desktop\2", dateGroup.Key.ToString("yyyy-MM-dd"));
+
+                    Directory.CreateDirectory(dir);
+                    foreach (var file in dateGroup)
+                    {
+                        string newPath = Path.Combine(dir, file.Name);
+                        File.Copy(file.FullName, newPath,true);
+                        File.Delete(file.FullName);
+
+                        newFileCount = files.Count();
+
+                        progressValue = (1 - (newFileCount / totalFileCount)) * 100 ;
+                        progressValueInt = (int)Math.Round(progressValue);
+
+                        progressBar2.Value = progressValueInt;
+                        progressBar2.Update();
+
+
+                    }
+                }
+                MessageBox.Show("File Upload Complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            }
+
+        private void Rd_packing_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSource.Text = @"G:\";
+            destination = @"\\designsvr1\Public\Slimline Packing";
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSource.Text = @"E:\";
+            destination = @"\\designsvr1\public\Shop Pictures";
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSource.Text = @"G:\";
+            destination = @"\\DESIGNSVR1\Slimline\Shop floor  operation video`s";
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSource.Text = @"E:\";
+            destination = @"\\designsvr1\Public\Dispatch pictures";
+        }
+
+        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSource.Text = @"E:\";
+            destination = @"\\designsvr1\Public\Welding pictures";
+        }
+    }
+    }
+
